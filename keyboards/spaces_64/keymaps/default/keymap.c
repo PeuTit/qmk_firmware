7865d39e75647d82540c94feaeafcc4ecc73563d
Titouan Guiochet <22703571+PeuTit@users.noreply.github.com>
2023-10-26 16:37:13 +0200

    Add general default keymap layout

--- keyboards/spaces_64/keymaps/default/keymap.c
+++ keyboards/spaces_64/keymaps/default/keymap.c
@@ -28,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [_BL] = LAYOUT(
-    KC_CAPS,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL, KC_BSPC,
+    KC_ESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL, KC_BSPC,
     KC_TAB,    KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,  KC_RBRC,
-    LCTL(KC_ESC),     KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,  KC_BSLS, KC_ENT,
+    KC_CAPS,     KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,  KC_BSLS, KC_ENT,
     SC_LSPO,  KC_GRAVE,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,   SC_RSPC,
     MO(_FL),    KC_LCTL,    KC_LALT,    KC_LGUI,  MO(_ML),  KC_SPC,  KC_RGUI,  KC_RALT,  KC_LCTL, MO(_FL)),
 
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
 [_ML] = LAYOUT(
-  _______,  _______, _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,  _______,  _______,
-  _______, KC_MS_BTN1, KC_MS_BTN3, KC_MS_BTN2,_______,_______,_______,_______,_______,_______,_______,  _______,  _______,
-  _______, KC_MS_LEFT, KC_MS_UP, KC_MS_DOWN, KC_MS_RIGHT,_______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______,  _______,  C(LOPT(KC_ENTER)),
-  _______, _______, _______,_______,_______,_______,_______,_______,C(LOPT(KC_LEFT)),C(LOPT(KC_DOWN)), C(LOPT(KC_UP)),  C(LOPT(KC_RIGHT)),  _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______,  _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______,        MO(_ML),_______,                        _______,  _______,  _______,  _______),
 };
